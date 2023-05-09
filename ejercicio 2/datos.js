d7800c4e437273079bd6c10a955c7062b8d4668e
juan carlos <jguayancastillo21@gmail.com>
2023-05-09 01:14:25 -0500

    agregado el ejercicio 2

---
+++ ejercicio 2/datos.js
@@ -0,0 +1,14 @@
+let nombre = "juan carlos";
+
+let edad = 28;
+
+let desarrollador = true;
+
+let fecha_nacimiento = new Date(1993,7,29);
+
+const libro_favorito={
+    "titulo": "el horror the dunwich",
+    "autor": "hp lovecraft",
+    "fecha": 1928,
+    "url": "www.libros_de_horror.com"
+}
